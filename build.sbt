7697a8a022368d2878ee9784492df13be13f6d7e
Gerard Downes <gdownes@gmail.com>
2021-03-09 23:53:38 +0000

    Adding futures

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "sync-scala-cli"
+name := "async-scala-cli"
 
 version := "0.1"
 
@@ -6,6 +6,6 @@ scalaVersion := "2.13.5"
 
 libraryDependencies += "org.slf4j" % "slf4j-api" % "1.5.6"
 libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.5.6"
-libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.2"
+libraryDependencies += "org.dispatchhttp" %% "dispatch-core" % "1.2.0"
 libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind" % "2.12.2"
 libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.2"
--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -2,24 +2,31 @@ import com.fasterxml.jackson.core.`type`.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.slf4j.LoggerFactory
-import scalaj.http.Http
+import dispatch._
+import Defaults._
 
 object Application extends App {
+  val logger = LoggerFactory.getLogger(getClass.getSimpleName)
+
   val mapper = new ObjectMapper()
   mapper.registerModule(DefaultScalaModule)
 
-  val logger = LoggerFactory.getLogger(getClass.getSimpleName)
   logger.info("Getting latest tender ids")
-  val tendersResponse = Http("https://api.tender-ni.com/tender").asString
-  val tenders = mapper.readValue(tendersResponse.body, new TypeReference[List[Map[String, String]]] {})
-  val tenderDescriptions = tenders.map(tender => getTenderDescription(tender("id")))
-  println(tenderDescriptions)
 
-  def getTenderDescription(id: String): String = {
-    logger.info("Getting tender details for tender id " + id)
-    val tenderResponse = Http(s"https://api.tender-ni.com/tender/${id}").asString
-    val tenderDetails = mapper.readValue(tenderResponse.body, new TypeReference[Map[String, Object]] {})
-    tenderDetails.getOrElse("description", "Unknown").toString
+  val tendersResponse = Http.default(url("https://api.tender-ni.com/tender") OK as.String)
+  tendersResponse.map(tendersResponse => {
+    val tenders = mapper.readValue(tendersResponse, new TypeReference[List[Map[String, String]]] {})
+    val tenderDescriptions = tenders.map(tender => getTenderDescription(tender("id")))
+    Future.sequence(tenderDescriptions).foreach(tenderDescriptions => println(tenderDescriptions))
+  })
+
+  def getTenderDescription(id: String): Future[String] = {
+    logger.info(s"Getting tender details for tender id $id")
+    val tenderResponse = Http.default(url(s"https://api.tender-ni.com/tender/$id") OK as.String)
+    tenderResponse.map(tenderResponse => {
+      val tender = mapper.readValue(tenderResponse, new TypeReference[Map[String, Object]] {})
+      tender("description").toString
+    })
   }
 }
 
